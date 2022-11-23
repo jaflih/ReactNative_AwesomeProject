f5e61254fad5fdc5ecdb366aad477da6b143cd44
jihaneh <jijih@hotmail.fr>
2022-11-23 18:25:36 +0100

    Add home page design

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,34 @@
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, SafeAreaView, FlatList, Text } from 'react-native';
+
+import { COLORS } from '../constants';
+import { NFTData } from '../constants/dummy';
+
+import HomeHeader from '../components/HomeHeader';
+import FocusedStatusBar from '../components/FocusedStatusBar';
+import NFTCard from '../components/NFTCard';
 
 const Home = () => {
-  return <Text>Home</Text>;
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar background={COLORS.primary} />
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            data={NFTData}
+            renderItem={({ item }) => <NFTCard data={item} />}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader />}
+          />
+        </View>
+        <View style={{ position: 'absolute', top: 0, bottom: 0, right: 0, left: 0, zIndex: -1 }}>
+          <View style={{ height: 300, background: COLORS.primary }} />
+          <View style={{ flex: 1, background: COLORS.white }} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default Home;
